80cc62ae9181118e52f4996d8e1abd8144ce6c74
Keenan Lee <keenan.lee@eagles.oc.edu>
2019-04-16 14:58:14 -0500

    Finished all tasks

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include "NumberList.h"
+#include <fstream>
 using namespace std;
 
 int main()
 {
 	NumberList list;
-	list.insertNode(10);
-	list.appendNode(40);
-	list.insertNode(30);
-	list.insertNode(25);
-	list.deleteNode(30);
+	//list.insertNode(10);
+	//list.appendNode(40);
+	//list.insertNode(30);
+	//list.insertNode(25);
+	//list.deleteNode(30);
+	//list.displayList();
+	fstream inFile;
+	inFile.open("Integers.txt");
+	int tempInt;
+	while (inFile >> tempInt)
+	{
+		list.insertNode(tempInt);
+	}
+	inFile.close();
 	list.displayList();
 	return 0;
 }
